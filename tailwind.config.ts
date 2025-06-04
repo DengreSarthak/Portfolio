2f9e4f4343057643fab773488c3c7cf7279d0125
Sarthak Dengre <134192334+DengreSarthak@users.noreply.github.com>
2025-06-04 22:40:49 +0530

    Error fixes

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+        vt323: ["VT323", "monospace"],
+        rajdhani: ["Rajdhani", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config; 
